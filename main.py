296d16ae80266993e86fc03026a91a43f702c725
Hircrown <221099175+Hircrown@users.noreply.github.com>
2025-07-17 21:50:08 +0200

    Functions use the path provided by the user and handle basic misinputs

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+import sys
 from stats import words_counter, char_counter, dict_to_ordered_list
 
+
 def get_book_text(path):
-    with open(path) as f:
-        file_content = f.read()
-        return file_content
+    try:
+        with open(path) as f:
+            file_content = f.read()
+            return file_content
+    except FileNotFoundError:
+        print("Can't find that book")
+        sys.exit(3)
+    except Exception as e:
+        print(e)
+        sys.exit(4)
 
 
 def print_report(book_path, total_words, letters_dict):
@@ -18,11 +27,23 @@ def print_report(book_path, total_words, letters_dict):
     print("============= END ===============")
 
 
+def sys_input():
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    elif len(sys.argv) == 2:
+        return sys.argv[1]
+    else:
+        print("Too many arguments! Usage: python3 main.py <path_to_book>")
+        sys.exit(2)
+
 
 def main():
-    text = get_book_text("books/frankenstein.txt")
+    path = sys_input()
+    text = get_book_text(path)
     words_num = words_counter(text)
     char_dict = char_counter(text)
-    print_report("books/frankenstein.txt", words_num, char_dict)
+    print_report(path, words_num, char_dict)
 
 main()
+
